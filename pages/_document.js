b52bf1843605c19160aa3b0616b40ababfc97fae
Tom Hooper <tomwbhooper@hotmail.com>
2023-04-05 09:32:54 +0100

    Update name

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Tom Hooper\'s Portfolio',
+    description: 'Built with Next.js and Vercel.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
